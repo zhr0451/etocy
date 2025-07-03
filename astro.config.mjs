e7f831392e5e508453e69ad2ad306b8664616e50
zhr0451 <1samokhvalov.zs@gmail.com>
2025-07-03 14:36:54 +0300

    Изменения главной страницы, настройка конфига

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [starlightThemeRapide()],
+			title: 'K20',
+			logo: {
+				src: '/home/zhrr/etocy/src/assets/logo.svg',
+			},
+			social: [{ icon: 'telegram', label: 'telegram', href: 'https://t.me/no_roleplaying' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Главная страница в разработке',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
 					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
