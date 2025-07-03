865bc7d4119a0cb50781e191b67549a65b1c9025
zhr0451 <1samokhvalov.zs@gmail.com>
2025-07-04 00:01:25 +0300

    Приведение сайта к общему внешнему виду

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,23 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeRapide from 'starlight-theme-rapide'
+import starlightBlogPlugin from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			plugins: [starlightThemeRapide()],
-			title: 'K20',
-			logo: {
-				src: '/home/zhrr/etocy/src/assets/logo.svg',
-			},
-			social: [{ icon: 'telegram', label: 'telegram', href: 'https://t.me/no_roleplaying' }],
-			sidebar: [
-				{
-					label: 'Главная страница в разработке',
-					items: [
-						// Each item here is one entry in the navigation menu.
-					],
-				},
+			plugins: [
+				starlightThemeRapide(),
+				starlightBlogPlugin()
+
 			],
-		}),
-	],
-});
+			title: 'K20',
+			description: 'Сайт семьи етосу',
+
+			logo: {
+				src: '/src/assets/logo.svg',
+			},
+		})
+	]
+})
